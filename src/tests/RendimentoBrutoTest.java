bd553e3b9db148f10166e898822f9b46327a4aef
Victor <guin-hu@hotmail.com>
2018-09-21 16:25:14 -0300

    Falta de implementação de unidade

---
+++ src/tests/RendimentoBrutoTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RendimentoBrutoTest {
+	
+	@Test
+	public void testRendimentoBruto() {
+		
+		Rendimento rendimento = new Rendimento(); {
+			
+		}
+	}
+	
+ /*
+  @Test
+	public void test2opasso_CadastrarRendimentoDuplicado() throws RendimentosNulosException {
+		String descricao = "Salario"; 
+		float valor = 3000f;
+		
+		System.out.println(2 + " " + irpf);
+		Rendimento r = new Rendimento(descricao, valor);
+		
+		assertTrue(irpf.cadastrarRendimento(r));
+		assertEquals(8000f, irpf.totalRendimentos(), 0f);
+		assertEquals(1, irpf.numRendimentos());
+	}
+  */
+
+}
